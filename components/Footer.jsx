f41f206eb0b6fe1e7448eb8e53b835238ff0c910
Nasir Hussain <nasir.swat.hussain@gmail.com>
2024-01-25 17:26:35 +0500

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='mb-10 px-4 text-center text-gray-500 '>
         <small className='mb-2 block text-xs'>
-            &copy; 2030 <a href="https://zaryabdev.com" target='_blank' className='underline text-gray-700 dark:text-white/60'>zaryabdev.com</a>. All rights reserved.
+            {/* &copy; 2030 <a href="https://zaryabdev.com" target='_blank' className='underline text-gray-700 dark:text-white/60'>zaryabdev.com</a>. All rights reserved. */}
         </small>
         <p className='text-xs'>
             <span className='font-semibold'>About this Website: </span> built with react and Next.js (App Router & Server Actions), Javascript, Tailwind CSS, Framer Motion, React Email & Resend.
